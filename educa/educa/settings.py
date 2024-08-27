cb454db3534e2f03acd5ce7d5d3fdce141675e1d
abhishek <abhishekgharami1998@gmail.com>
2024-08-27 14:28:04 +0530

    All config done .

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.app.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
