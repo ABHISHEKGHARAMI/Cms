300e0e722ccc5d0d2f771ab0210cdd3b0ab9e5f9
abhishek <abhishekgharami1998@gmail.com>
2024-08-27 14:43:17 +0530

    Added the Subjects, Course and Module model for the cms .

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# Model for the Subjects
+
+class Subject(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150,unique=True)
+    
+    class Meta:
+        ordering = ['title']
+        
+        
+    # str method
+    def __str__(self):
+        return self.title
+    
+    
+    
+# model for the course
+
+class Course(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='course_created',
+                              on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,
+                                related_name='courses',
+                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150,unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    # meta class
+    class Meta:
+        ordering = ['-created']
+        
+        
+    # str method
+    def __str__(self):
+        return self.title
+    
+    
+# class for the module
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    
+    # str method
+    def __str__(self):
+        return self.title
+    
