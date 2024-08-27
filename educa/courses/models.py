43821bab9b1fcc2f1411af7dd6d8943ca06f10f5
abhishek <abhishekgharami1998@gmail.com>
2024-08-27 15:26:04 +0530

    # mod

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 
 
