21f6583df7c9c9d2b0e77b84470a3b09d85ca627
abhishek <abhishekgharami1998@gmail.com>
2024-09-02 16:29:19 +0530

    Adding the views for the Module contest list view for the

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -169,3 +169,17 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list',module.id)
+    
+# module_content_list view for the user
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+    
+    
+    def get(self,request,module_id):
+        module = get_object_or_404(Module,
+                                   id=module_id,
+                                   course__owner=request.owner)
+        return self.render_to_response({
+            'module':module
+        })
+        
