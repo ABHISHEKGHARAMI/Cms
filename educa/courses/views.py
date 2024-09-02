9d5cbdf3a6655a8aebc06e8af0b3411dc3b4e10c
abhishek <abhishekgharami1998@gmail.com>
2024-09-02 17:18:38 +0530

    Issue attribute error at  the item at 42.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -178,7 +178,7 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self,request,module_id):
         module = get_object_or_404(Module,
                                    id=module_id,
-                                   course__owner=request.owner)
+                                   course__owner=request.user)
         return self.render_to_response({
             'module':module
         })
