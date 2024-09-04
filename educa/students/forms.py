564461b876d061497445912d4c14dbc327299e52
abhishek <abhishekgharami1998@gmail.com>
2024-09-04 10:15:54 +0530

    Adding the course enrollment form for the student .

---
+++ educa/students/forms.py
@@ -0,0 +1,8 @@
+# student form for go to course
+from django import forms
+from courses.models import Course
+
+class CourseEnrollForm(forms.Form):
+    course = forms.ModelChoiceField(
+        queryset=Course.objects.all(),
+        widget=forms.HiddenInput)
