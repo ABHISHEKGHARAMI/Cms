681524d27657e964a3a84390a803253a2f078b83
abhishek <abhishekgharami1998@gmail.com>
2024-09-04 10:33:26 +0530

    adding it to course detail view .

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -13,6 +13,7 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.db.models import Count
 from .models import Subject
 from django.views.generic.detail import DetailView
+from students.forms import CourseEnrollForm
 # Create your views here.
 
 class CourseListView(ListView):
@@ -244,3 +245,12 @@ class CourseListView(TemplateResponseMixin,View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+    
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+            initial={
+                'course' : self.object
+            }
+        )
+        return context
