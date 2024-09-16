1dffc8c89ec68389a16ca4578307caa435c0cb4a
abhishek <abhishekgharami1998@gmail.com>
2024-09-16 21:52:34 +0530

    Added the views for the rest Framework .

---
+++ educa/courses/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from courses.models import Subject
+from courses.api.serializers import SubjectSerializer
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    
+    
+class SubjectDetailView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
