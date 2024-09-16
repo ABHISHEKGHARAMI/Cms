3dba383110311fab7822d1bb4e472dc5004ba8f1
abhishek <abhishekgharami1998@gmail.com>
2024-09-16 15:44:53 +0530

    Adding the storing with the cache .

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -14,6 +14,7 @@ from django.db.models import Count
 from .models import Subject
 from django.views.generic.detail import DetailView
 from students.forms import CourseEnrollForm
+from django.core.cache import cache
 # Create your views here.
 
 class CourseListView(ListView):
@@ -223,9 +224,12 @@ class CourseListView(TemplateResponseMixin,View):
     template_name = 'courses/course/list.html'
     
     def get(self,request,subject=None):
-        subjects = Subject.objects.annotate(
-            total_courses = Count('courses')
-        )
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(
+                total_courses=Count('courses')
+            )
+            cache.set('all_subjects',subjects)
         
         courses = Course.objects.annotate(
             total_modules = Count('modules')
