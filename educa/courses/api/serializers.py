4351a06b43d55e36b478a6161f02d3e8482b5160
abhishek <abhishekgharami1998@gmail.com>
2024-09-17 12:47:40 +0530

    Added the Course serializer.

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from  courses.models import Subject
+from  courses.models import Subject , Course
 
 # class for serializer
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id','title','slug']
+        
+        
+# serializer for the course
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner',
+                  'modules']
         
