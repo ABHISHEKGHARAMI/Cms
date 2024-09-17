b33f400893ba826ad2c0a1366f03eb7fa15ae777
abhishek <abhishekgharami1998@gmail.com>
2024-09-17 14:42:45 +0530

    Adding the swagger  for the post.

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -21,12 +21,19 @@ class SubjectDetailView(generics.RetrieveAPIView):
 class CourseEnrollView(APIView):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    
+    @swagger_auto_schema(
+        operation_description="Enroll in a specific course",
+        request_body=None,  # Adjust if you expect a request body
+        responses={200: openapi.Response(description="Enrollment status", examples={
+                                         'application/json': {'enrolled': True}})},
+    )
     def post(self,request,pk,format=None):
         course = get_object_or_404(Course,pk=pk)
         course.students.add(request.user)
         return Response({
             'enrolled' : True
-        })
+        }, status=status.HTTP_200_OK)
         
     
 
