ebb353f4f0c4d475ea15ccd906afbeced385dcf1
abhishek <abhishekgharami1998@gmail.com>
2024-09-17 14:40:32 +0530

    Adding the swagger model .

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path
 from . import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
 app_name = 'courses'
+
+# schema for the swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Courses API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@courses.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -15,5 +31,11 @@ urlpatterns = [
     path('courses/<int:pk>/enroll/',
          views.CourseEnrollView.as_view(),
          name='course_enroll'),
+    # Swagger documentation URLs
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    # Redoc documentation URLs (optional)
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
     
 ]
