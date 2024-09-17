3c3f46ddab474cd25c5d45792a6edc048270014d
abhishek <abhishekgharami1998@gmail.com>
2024-09-17 12:55:53 +0530

    Adding the course and module serializer .

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from  courses.models import Subject , Course
+from  courses.models import Subject , Course , Module
 
 # class for serializer
 class SubjectSerializer(serializers.ModelSerializer):
@@ -7,12 +7,23 @@ class SubjectSerializer(serializers.ModelSerializer):
         model = Subject
         fields = ['id','title','slug']
         
+# serializer for module
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+        
         
 # serializer for the course
 class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
     class Meta:
         model = Course
         fields = ['id', 'subject', 'title', 'slug',
                   'overview', 'created', 'owner',
                   'modules']
+        
+        
         
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from courses.models import Subject , Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -10,5 +10,8 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    
+    
+
     
     
