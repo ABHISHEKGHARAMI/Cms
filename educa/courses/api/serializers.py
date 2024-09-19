55a35e5cf76ceeb4abfe631042d0fef067e70b8d
abhishek <abhishekgharami1998@gmail.com>
2024-09-19 16:39:06 +0530

    Adding the content serializer .

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from  courses.models import Subject , Course , Module
+from  courses.models import Subject , Course , Module, Content
 
 # class for serializer
 class SubjectSerializer(serializers.ModelSerializer):
@@ -25,5 +25,19 @@ class CourseSerializer(serializers.ModelSerializer):
                   'overview', 'created', 'owner',
                   'modules']
         
+        
+# serializer for item fields to render the item
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self,value):
+        return value.render()
+    
+    
+
+# content serializer
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+    class Meta:
+        model = Content
+        fields = ['order','item']
         
         
