e43f8d813bd6db8d645888198e45c8fd602f3673
abhishek <abhishekgharami1998@gmail.com>
2024-09-22 11:55:38 +0530

    Adding the username and password.

--- api-examples/enroll_all.py
+++ api-examples/enroll_all.py
@@ -1,6 +1,8 @@
 import requests
 
 # base url
+username = 'Test002@'
+password = 'Gharami1998@'
 
 base_url = 'http://127.0.0.1:8000/api/'
 
@@ -13,6 +15,15 @@ courses = r.json()
 available_courses = ','.join(course['title'] for course in courses)
 
 print(f'Available courses : {available_courses}')
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+    r = requests.post(f'{base_url}/course/{course_id}/enroll/',
+                      auth=(username,password))
+    
+    if r.status_code == 200:
+        print(f'Successfully enrolled : {course_title}')
 
 
 
