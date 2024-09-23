917689b341a78b15c9d81da104f3b43084cc5f42
abhishek <abhishekgharami1998@gmail.com>
2024-09-23 21:14:24 +0530

    # Mod

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('students/',include('students.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/', include('courses.api.urls', namespace='api')),
-    path('chat/',include('chat.urls',namespace='chat')),
+    path('chat/', include('chat.urls', namespace='chat')),
 ]
 
 
