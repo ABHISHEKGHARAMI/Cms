72d8eb1c05948ddb7e96c1c7ecbbc4ec2b1ffd45
abhishek <abhishekgharami1998@gmail.com>
2024-09-23 21:11:07 +0530

    # Mod

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -4,12 +4,13 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 @login_required
-def course_chat_room(request,course_id):
+def course_chat_room(request, course_id):
     try:
+        # retrieve course with given id joined by the current user
         course = request.user.courses_joined.get(id=course_id)
-    except HttpResponseForbidden:
+    except:
+        # user is not a student of the course or course does not exist
         return HttpResponseForbidden()
-    return render(request,'chat/room.html',{
-        'course': course
-    })
+    return render(request, 'chat/room.html', {'course': course})
