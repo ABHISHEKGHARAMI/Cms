00552d05ca6b0162b5c36c0e731688dd75325a8b
abhishek <abhishekgharami1998@gmail.com>
2024-09-23 21:21:22 +0530

    # mod

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -21,4 +21,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
 })
