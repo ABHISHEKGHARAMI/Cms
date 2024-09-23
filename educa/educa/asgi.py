d98857adb853eb865b703308103a9d17e198e186
abhishek <abhishekgharami1998@gmail.com>
2024-09-23 19:46:41 +0530

    Added the asgi application through the channels .

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
+django_asgi_application = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': django_asgi_application,
+})
