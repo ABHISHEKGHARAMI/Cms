995dcd8fa85b9e6d0ac4ecd1b66e3ffc94b52fdc
abhishek <abhishekgharami1998@gmail.com>
2024-09-23 21:01:09 +0530

    # mod

--- educa/chat/routing.py
+++ educa/chat/routing.py
@@ -2,8 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 
-
-# route for the asgi server
 websocket_urlpatterns = [
     re_path(r'ws/chat/room/(?P<course_id>\d+)/$',
             consumers.ChatConsumer.as_asgi()),
