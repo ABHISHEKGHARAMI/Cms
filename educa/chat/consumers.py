304276b62df4e64bdee94e3d487f19ab2d256eb8
abhishek <abhishekgharami1998@gmail.com>
2024-09-25 11:44:47 +0530

    # adding the timezone and user for the testing .

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -3,6 +3,7 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -23,12 +24,15 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
         # send message to the group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message' : message
+                'message' : message,
+                'user' : self.user.username,
+                'datetime': now.isoformat()
             }
         )
         
