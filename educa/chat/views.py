179f203dbb22be7ad5539f378cf4f55151d2ffe8
abhishek <abhishekgharami1998@gmail.com>
2024-09-26 11:38:33 +0530

    Adding the perssisted message .

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -15,6 +15,8 @@ def course_chat_room(request, course_id):
         # user is not a student of the course or course does not exist
         return HttpResponseForbidden()
     
-    latest_messages = course.chat_messages.selected_related('user').order_by('-id')[:5]
+    latest_messages = course.chat_messages.select_related(
+        'user'
+    ).order_by('-id')[:5]
     latest_messages = reversed(latest_messages)
     return render(request, 'chat/room.html', {'course': course,'latest_messages':latest_messages})
