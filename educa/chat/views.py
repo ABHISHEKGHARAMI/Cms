0b4f7c6e539fcfdd53a4fe7ea9cf81726e0abfad
abhishek <abhishekgharami1998@gmail.com>
2024-09-26 11:31:13 +0530

    Retriving the latest message of the user .

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from courses.models import Course
 
 # Create your views here.
 
@@ -13,4 +14,7 @@ def course_chat_room(request, course_id):
     except:
         # user is not a student of the course or course does not exist
         return HttpResponseForbidden()
-    return render(request, 'chat/room.html', {'course': course})
+    
+    latest_messages = course.chat_messages.selected_related('user').order_by('-id')[:5]
+    latest_messages = reversed(latest_messages)
+    return render(request, 'chat/room.html', {'course': course,'latest_messages':latest_messages})
