341cfe104d414e8a81c37ace7c126308a4d38440
abhishek <abhishekgharami1998@gmail.com>
2024-09-26 10:37:51 +0530

    Coverting all the asynchronus task for the consumer and receiver .

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -2,32 +2,32 @@
 
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+    async def connect(self):
         self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'char_{self.id}'
         # join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,self.channel_name
         )
         # accept connection
-        self.accept()
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.accept()
+    async def disconnect(self, close_code):
+        await async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,self.channel_name
         )
     # receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         now = timezone.now()
         # send message to the group
-        async_to_sync(self.channel_layer.group_send)(
+        await async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -38,5 +38,5 @@ class ChatConsumer(WebsocketConsumer):
         )
         
     # receive message from the room group
-    def chat_message(self,event):
-        self.send(text_data=json.dumps(event))
+    async def chat_message(self,event):
+        await self.send(text_data=json.dumps(event))
