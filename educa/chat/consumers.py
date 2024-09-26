da829df631f1dd062ada5cfbc6790bf4ce44c694
abhishek <abhishekgharami1998@gmail.com>
2024-09-26 11:20:50 +0530

    # mod

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -4,7 +4,7 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.utils import timezone
-
+from chat.models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -21,6 +21,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(
             self.room_group_name,self.channel_name
         )
+        
+    # persist message to database
+    async def persist_message(self, message):
+        await Message.objects.acreate(
+            user=self.user, course_id=self.id, content=message
+        )
     # receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -36,6 +42,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'datetime': now.isoformat()
             }
         )
+        
+        # persist message
+        await self.persist_message(message)
         
     # receive message from the room group
     async def chat_message(self,event):
