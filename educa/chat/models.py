7d94ad63201810678bb6ea10fa192a331825a301
abhishek <abhishekgharami1998@gmail.com>
2024-09-26 10:58:58 +0530

    Adding the models for storing the result .

--- educa/chat/models.py
+++ educa/chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+# creating the model for the storing the message in the database
+class Message(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.PROTECT,
+                             related_name='chat_messages')
+    course = models.ForeignKey('courses.Course',
+                               on_delete=models.PROTECT,
+                               related_name='chat_messages')
+    content = models.TextField()
+    sent_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user} on {self.course} on {self.sent_on}"
