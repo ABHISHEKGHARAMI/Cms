7bddacb41569d260ca06a9c0ea7e018c977ecba7
abhishek <abhishekgharami1998@gmail.com>
2024-09-27 11:49:46 +0530

    Adding the local enviroment for the repo .

--- educa/educa/settings/base.py
+++ educa/educa/settings/base.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-qqsl3$x(cwdj-s8j6*zoxbl727&1r=m&cl0qi2@u6(wnk8bty_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -84,15 +83,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'educa.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
