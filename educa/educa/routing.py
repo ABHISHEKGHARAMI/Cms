1c5602417f40353d8b1366291b7e8143f7f127e5
abhishek <abhishekgharami1998@gmail.com>
2024-10-02 15:02:23 +0530

    Allowing for the chat server .

--- educa/educa/routing.py
+++ educa/educa/routing.py
@@ -3,6 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 # Import WebSocket routes from the chat app
 from chat.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
 
 # Define the ASGI application for Django (HTTP requests)
 django_asgi_app = get_asgi_application()
@@ -10,9 +11,10 @@ django_asgi_app = get_asgi_application()
 # Define how to handle different protocol types (HTTP and WebSocket)
 application = ProtocolTypeRouter({
     'http': django_asgi_app,  # HTTP requests handled by Django
-    'websocket': AuthMiddlewareStack(  # WebSocket requests handled by Channels
-        URLRouter(
-            websocket_urlpatterns  # WebSocket routes from chat app
-        )
-    ),
+    'websocket': AllowedHostsOriginValidator(
+                    AuthMiddlewareStack(  # WebSocket requests handled by Channels
+                            URLRouter(
+                                websocket_urlpatterns  # WebSocket routes from chat app
+                            )
+       )),
 })
