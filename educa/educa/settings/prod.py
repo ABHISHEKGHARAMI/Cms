d39a4e425b1e0189d68757cf4f356d7e6f339afe
abhishek <abhishekgharami1998@gmail.com>
2024-10-02 14:32:35 +0530

    Adding the settings .

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -29,3 +29,9 @@ DATABASES = {
 REDIS_URL = 'redis://cache:6379'
 CACHES['default']['LOCATION'] = REDIS_URL
 CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+
+# Security
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
