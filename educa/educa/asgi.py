4116eb9cbba1517c7dab33f283319e97ca67b610
abhishek <abhishekgharami1998@gmail.com>
2024-10-04 11:48:34 +0530

    # mod for the asgi .

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -8,15 +8,25 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
-
+from channels.security.websocket import AllowedHostsOriginValidator
+from chat.routing import websocket_urlpatterns  # Importing websocket routes
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
+# HTTP protocol handled by Django ASGI
 django_asgi_app = get_asgi_application()
 
-
+# ASGI application to handle both HTTP and WebSocket
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,  # Handle HTTP requests
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns  # Handle WebSocket routes
+            )
+        )
+    ),
+})
